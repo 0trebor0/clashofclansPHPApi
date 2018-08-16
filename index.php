89f36661ee561562b5adceb59682f273de569d1e
0trebor0 <robertsantos98@hotmail.co.uk>
2018-08-16 11:33:13 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+require(__DIR__.'/class.php');
+$api = new api;
+//print_r($api->getClan("#YOURCLANTAG"));
+//print_r($api->getPlayer("#YOURPLAYERTAG"));
+//print_r($api->getClanMembers("#YOURCLANTAG"));
+//print_r($api->getClanWarLog("#YOURCLANTAG"));
+//print_r($api->getClanCurrentWar("#YOURCLANTAG"));
+//print_r($api->getLeagues());
